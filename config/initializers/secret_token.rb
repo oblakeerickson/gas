a882409645d5cf54bdc491aa997d7709455b7e0d
Blake Erickson <o.blakeerickson@gmail.com>
2014-03-27 07:16:24 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gas::Application.config.secret_key_base = '73fad28506e3ca318ebb501dd56c1e015cdf743277c01d556c9250ea94ba2d2c21fd3884c17dfbc82fdebf8a466ed6eb42ff2d508d9cc3cc213f6d663215c54e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gas::Application.config.session_store :cookie_store, key: '_gas_session'
